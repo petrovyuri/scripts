b8ac19b75069a8028b01611558b0a0845ada605f
petrovyuri <mr.developer>
2023-03-04 13:59:23 +0300

    up docker

--- docker.sh
+++ docker.sh
@@ -6,3 +6,6 @@ docker-compose build --no-cache
 
 # Запуск докер компос
 docker-compose up
+
+# Для создания уникального контейнера на основен тек. времени
+docker-compose build --build-arg CACHEBUST=$(Get-Date -UFormat %s)
