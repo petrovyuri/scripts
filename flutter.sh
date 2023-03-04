f18ee4b22cd60c2f5e5060cbe3b80cf3eeb2c562
petrovyuri <mr.developer>
2023-03-04 12:17:19 +0300

    first commit

---
+++ flutter.sh
@@ -0,0 +1,2 @@
+# Запуск кодогенерации и устранение конфликтов
+flutter pub run build_runner build --delete-conflicting-outputs
---
+++ python.sh
@@ -0,0 +1,20 @@
+# Запуск сервера Django
+python manage.py runserver
+
+# Сгенерировать новый requirements.txt
+pipenv freeze > requirements.txt
+
+# Создать новый проект
+django-admin startproject auth_api .
+
+# Установка django в виртуальное окружение
+pipenv install django
+
+# Создание миграций
+python manage.py makemigrations
+
+# Запуск миграции
+python manage.py migrate
+
+# Создание супер пользователя в Django
+python manage.py createsuperuser
