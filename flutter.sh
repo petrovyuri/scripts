73d856d4498c41bc14a4a4f221bae1da5741a127
petrovyuri <mr.developer>
2023-03-07 14:04:18 +0300

    update ios

--- flutter.sh
+++ flutter.sh
@@ -1,2 +1,5 @@
 # Запуск кодогенерации и устранение конфликтов
 flutter pub run build_runner build --delete-conflicting-outputs
+
+# Сборка ios без подписи с флавором
+flutter build ios --release --flavor dev --no-codesign
